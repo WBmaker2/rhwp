149cf9195efa8161e198a95d9c4f915774a10d37
WBmaker2 <103619091+WBmaker2@users.noreply.github.com>
2026-07-25 11:19:41 +0900

    feat: expose document API contracts

---
+++ services/document-api/src/index.ts
@@ -0,0 +1,18 @@
+export {
+  ParseLease,
+  type LeaseResult,
+  type ParseLeaseState,
+  type ParseLeaseStore,
+} from './parse-lease.js'
+export { exportPath, sourcePath } from './storage-paths.js'
+export {
+  createCompleteUploadHandler,
+  type ApiRequest,
+  type ApiResponse,
+  type CompleteUploadDependencies,
+  type DocumentRole,
+} from './routes/complete-upload.js'
+export {
+  createExportHwpxHandler,
+  type ExportHwpxDependencies,
+} from './routes/export-hwpx.js'
